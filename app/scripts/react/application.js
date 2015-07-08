6d86beeab859eb3f411753ddfa8164bdfa323c05
Sergey Laptev <iamsergeylaptev@gmail.com>
2015-07-08 16:34:21 +0300

    Update bootstrap data from backend

--- app/scripts/react/application.js
+++ app/scripts/react/application.js
@@ -2,6 +2,21 @@ import { createRedux } from 'redux';
 import { Provider } from 'redux/react';
 import * as stores from './stores';
 
+// Bootstraping serverside data
+let data = {};
+if (global.__data) {
+  const { design } = global.__data;
+
+  data = {
+    design: {
+      current: design,
+      currentSaved: design,
+      unsavedFields: {},
+      isSaving: false
+    }
+  };
+}
+
 // Unless we have no one common component, we will be pass <Provider /> global redux
 // instance
-global.redux = createRedux(stores, global.__data || {});
+global.redux = createRedux(stores, data);
