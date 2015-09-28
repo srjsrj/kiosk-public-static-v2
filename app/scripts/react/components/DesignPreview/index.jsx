41cbc8d42ec1776f4bf8baf0c95bfb084f3e95da
Sergey Laptev <iamsergeylaptev@gmail.com>
2015-09-28 18:37:48 +0300

    Design Settings

--- app/scripts/react/components/DesignPreview/index.jsx
+++ app/scripts/react/components/DesignPreview/index.jsx
@@ -6,49 +6,101 @@ import assign from 'react/lib/Object.assign';
 import tinycolor from 'tinycolor2';
 import connectToRedux from '../HoC/connectToRedux';
 
+      // "mainPageProductsInRow": 2,
+      // "mainPageRows": 5,
+      // "mainPageInstagram": true,
+      // "mainPageSlider": false,
+      // "mainPageBanner": true,
+      // "mainPageFilter": true,
+      // "categoryPageProductsInRow": 2,
+      // "categoryPageRows": 5,
+      // "categoryPageFilter": true,
+      // "productPagePhoto": "aside",
+      // "productPageSimilarProducts": false,
+      // "logoUrl": "http://assets.kiiiosk.ru/uploads/vendor/logo/5/logo.svg",
+      // "pageBgUrl": null,
+      // "pageBgColor": "#6c7a89",
+      // "feedBgColor": "#000000",
+      // "feedTransparency": 0,
+      // "fontColor": "#000000",
+      // "fontFamily": "helvetica",
+      // "fontSize": "md",
+
 const _rules = {
-  pageBgColor(value) {
-    return {
-      '.b-page': {
-        'background-color': value
-      }
-    };
-  },
-  pageBgUrl(value) {
-    return {
-      '.b-page': {
-        'background-image': value ? `url("${value}")` : 'none'
-      }
-    };
-  },
-  feedBgColor(value) {
-    return {
-      dep: 'feedTransparency',
-      rule(depValue) {
-        return {
-          '.b-page__content__inner': {
-            'background-color': tinycolor(value).setAlpha(1 - depValue).toRgbString()
+  // welcome: {
+  //   mainPageRows(value) {
+  //     return {
+  //       '.b-page .b-item-list:nth-child(-n+1)': {
+  //         display: 'none'
+  //       },
+  //     };
+  //   }
+  // },
+  // categories: {
+  //   categoryPageRows(value) {
+
+  //   }
+  // },
+  common: {
+    pageBgColor(value) {
+      return {
+        '.b-page': {
+          'background-color': value
+        }
+      };
+    },
+    pageBgUrl(value) {
+      return {
+        '.b-page': {
+          'background-image': value ? `url("${value}")` : 'none'
+        }
+      };
+    },
+    feedBgColor(value) {
+      return {
+        dep: 'feedTransparency',
+        rule(depValue) {
+          return {
+            '.b-page__content__inner': {
+              'background-color': tinycolor(value).setAlpha(1 - depValue).toRgbString()
+            }
           }
         }
-      }
-    };
-  },
-  fontColor(value) {
-    return {
-      '.b-page': {
-        'color': value
-      }
-    };
+      };
+    },
+    fontColor(value) {
+      return {
+        '.b-page': {
+          'color': value
+        }
+      };
+    },
   }
 };
 const _states = {
-  fontFamily: 'b-page_ff',
-  fontSize: 'b-page_fs',
-  productsInRow: 'b-page_cols',
-  productPagePhoto: 'b-page_layout-photo',
+  welcome: {
+    mainPageProductsInRow: 'b-page_cols',
+  },
+  categories: {
+    categoryPageProductsInRow: 'b-page_cols',
+  },
+  products: {
+    productPagePhoto: 'b-page_layout-photo',
+  },
+  common: {
+    fontFamily: 'b-page_ff',
+    fontSize: 'b-page_fs',
+  },
 };
 const _switchableStates = {
-  // productLayoutBigpic: 'b-page_layout-bigpic'
+  products: {
+    productPageSimilarProducts: 'b-page_layout-similar-products',
+  },
+  common: {
+    mainPageBanner: 'b-page_layout-banner',
+    mainPageInstagram: 'b-page_layout-instagram',
+    mainPageSlider: 'b-page_layout-slider',
+  },
 };
 
 @connect((state) => ({
@@ -56,7 +108,10 @@ const _switchableStates = {
 }))
 class DesignPreview {
   static propTypes = {
-    design: PropTypes.object.isRequired
+    design: PropTypes.object.isRequired,
+    pageType: PropTypes.oneOf([
+      'welcome', 'categories', 'products'
+    ]).isRequired,
   }
   componentDidMount() {
     this.sheet = jss.createStyleSheet({}, { named: false }).attach();
@@ -69,34 +124,46 @@ class DesignPreview {
     this.sheet.detach();
     this.sheet = null;
   }
-  render() {
-    return null;
-  }
   apply(design) {
-    const states = this.getStates(design, _states);
-    const switchableStates = this.getStates(design, _switchableStates);
-    const cssRules = this.getRules(design);
-    const pageClasses = this.getPageClasses(design, {...states, ...switchableStates});
+    const { pageType } = this.props;
+    const statesValues = this.getStatesValues(design, _states, pageType);
+    const switchableStatesValues = this.getStatesValues(design, _switchableStates, pageType);
+    const pageClasses = this.getPageClasses(design, { ...statesValues, ...switchableStatesValues });
+    const cssRules = this.getRules(design, pageType);
     const page = this.getElements().page;
 
     this.sheet.addRules(cssRules);
     page.className = pageClasses.join(' ');
     this.emitChangeEvents();
   }
-  getStates(design, states) {
-    return design.reduce((previous, value, property) => {
-      const state = states[property];
+  getStatesValues(design, states, pageType) {
+    const statesForPageType = this.getStatesByPageType(states, pageType);
 
+    return design.reduce((previous, value, property) => {
+      const state = statesForPageType[property];
       if (state) {
         previous[state] = value;
       }
-
       return previous;
     }, {});
   }
-  getRules(design) {
+  getStatesByPageType(states, pageType) {
+    return {
+      ...states.common,
+      ...states[pageType],
+    };
+  }
+  getRulesByPageType(rules, pageType) {
+    return {
+      ...rules.common,
+      ...rules[pageType],
+    };
+  }
+  getRules(design, pageType) {
+    const rulesForPageType = this.getRulesByPageType(_rules, pageType);
+
     return design.reduce((previous, value, property) => {
-      const rule = _rules[property];
+      const rule = rulesForPageType[property];
 
       if (rule) {
         const cssRule = rule(value);
@@ -127,7 +194,7 @@ class DesignPreview {
     Object.keys(states).reduce(function(previous, key) {
       if (states.hasOwnProperty(key)) {
         const value = states[key];
-        // If value is true or can be converted to boolean true
+
         if (value) {
           previous.push(value !== true ? key + '-' + value : key);
         }
@@ -144,6 +211,9 @@ class DesignPreview {
   }
   emitChangeEvents() {
     $(document).trigger('updateProductImages');
+  }
+  render() {
+    return null;
   }
 }
 
