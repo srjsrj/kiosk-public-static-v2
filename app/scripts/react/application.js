c16c47075447f2fbc2db0bb0288224de68837d06
Sergey Laptev <iamsergeylaptev@gmail.com>
2015-10-22 15:25:30 +0300

    Restore custom css comments

--- app/scripts/react/application.js
+++ app/scripts/react/application.js
@@ -18,7 +18,7 @@ if (global.gon.__data) {
 }
 
 global.Kiosk = {
-  version: '0.0.7',
+  version: '0.0.8',
 };
 
 // Unless we have no one common component, we will be pass <Provider /> global redux
