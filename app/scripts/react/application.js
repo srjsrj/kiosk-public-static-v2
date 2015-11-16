55cd8595773055f698f4bfe4cdab5ebffa4153a3
Andrey Sidorov <vozdelaluz@gmail.com>
2015-11-16 16:56:56 +0300

    [#108161960] Mobile cart fix

--- app/scripts/react/application.js
+++ app/scripts/react/application.js
@@ -18,7 +18,7 @@ if (global.gon.__data) {
 }
 
 global.Kiosk = {
-  version: '0.0.33',
+  version: '0.0.34',
 };
 
 // Unless we have no one common component, we will be pass <Provider /> global redux
