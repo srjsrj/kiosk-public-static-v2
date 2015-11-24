dcba0a68e759a56a3995b0724407fe7413158a0b
Andrey Sidorov <vozdelaluz@gmail.com>
2015-11-24 18:38:29 +0300

    [#107270766] Password field styles

--- app/scripts/react/application.js
+++ app/scripts/react/application.js
@@ -18,7 +18,7 @@ if (global.gon.__data) {
 }
 
 global.Kiosk = {
-  version: '0.0.55',
+  version: '0.0.56',
 };
 
 // Unless we have no one common component, we will be pass <Provider /> global redux
