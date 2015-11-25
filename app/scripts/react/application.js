393673d113469e889d89d2bb3dcbd89b780be7d0
Sergey Laptev <iamsergeylaptev@gmail.com>
2015-11-25 10:17:56 +0300

    ProductBlock details [#107297868]

--- app/scripts/react/application.js
+++ app/scripts/react/application.js
@@ -18,7 +18,7 @@ if (global.gon.__data) {
 }
 
 global.Kiosk = {
-  version: '0.0.59',
+  version: '0.0.60',
 };
 
 // Unless we have no one common component, we will be pass <Provider /> global redux
--- dist/scripts/public.prerender.development.js
+++ dist/scripts/public.prerender.development.js
@@ -1314,9 +1314,13 @@ Object.defineProperty(exports, '__esModule', {
 
 var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
+var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; desc = parent = getter = undefined; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 var _react = require('react');
 
@@ -1338,9 +1342,13 @@ var _ProductPrices = require('../ProductPrices');
 
 var _ProductPrices2 = _interopRequireDefault(_ProductPrices);
 
-var ProductBlock = (function () {
+var ProductBlock = (function (_Component) {
+  _inherits(ProductBlock, _Component);
+
   function ProductBlock() {
     _classCallCheck(this, _ProductBlock);
+
+    _get(Object.getPrototypeOf(_ProductBlock.prototype), 'constructor', this).apply(this, arguments);
   }
 
   _createClass(ProductBlock, [{
@@ -1373,23 +1381,26 @@ var ProductBlock = (function () {
               { className: 'b-item__name' },
               product.title
             ),
+            !!product.short_details && _react2['default'].createElement(
+              'div',
+              { className: 'b-item__details' },
+              product.short_details
+            ),
             _react2['default'].createElement(_ProductPrices2['default'], { product: product })
           )
         )
       );
     }
-  }], [{
-    key: 'propTypes',
-    value: {
-      product: _react.PropTypes.object.isRequired
-    },
-    enumerable: true
   }]);
 
   var _ProductBlock = ProductBlock;
   ProductBlock = (0, _HoCMakeTranslatable2['default'])(ProductBlock) || ProductBlock;
   return ProductBlock;
-})();
+})(_react.Component);
+
+ProductBlock.propTypes = {
+  product: _react.PropTypes.object.isRequired
+};
 
 exports['default'] = ProductBlock;
 module.exports = exports['default'];
