3d48baab8914f6f5a53d5a9acf0564b915bf6d76
Sergey Laptev <iamsergeylaptev@gmail.com>
2015-12-18 16:38:37 +0300

    Image relative size

--- app/scripts/react/schemas/product.js
+++ app/scripts/react/schemas/product.js
@@ -1,11 +1,13 @@
 import { PropTypes } from 'react';
+import image from './image';
 import money from './money'
 import good from './good';
 
 export default PropTypes.shape({
   id: PropTypes.number.isRequired,
   has_ordering_goods: PropTypes.bool.isRequired,
-  index_image_url: PropTypes.string.isRequired,
+  index_image: image,
+  second_image: image,
   is_label_new: PropTypes.bool.isRequired,
   is_run_out: PropTypes.bool.isRequired,
   is_sale: PropTypes.bool.isRequired,
