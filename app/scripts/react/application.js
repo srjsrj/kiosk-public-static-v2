5b0ca64654f9c5da3d7b3fa4a0707e7bb5b48130
Sergey Laptev <iamsergeylaptev@gmail.com>
2015-12-24 10:22:30 +0300

    ScrollToTop style improvements

--- app/scripts/react/application.js
+++ app/scripts/react/application.js
@@ -18,7 +18,7 @@ if (global.gon.__data) {
 }
 
 global.Kiosk = {
-  version: '0.0.118',
+  version: '0.0.119',
 };
 
 // Unless we have no one common component, we will be pass <Provider /> global redux
