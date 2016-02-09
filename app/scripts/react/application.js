a2f116560a205f6d970a53d99a12279c51f8f33e
Ananev Alexander <minoru.null@gmail.com>
2016-02-09 17:09:21 +0500

    fix not available button color text in black theme

--- app/scripts/react/application.js
+++ app/scripts/react/application.js
@@ -18,7 +18,7 @@ if (global.gon.__data) {
 }
 
 global.Kiosk = {
-  version: '0.0.157',
+  version: '0.0.161',
 };
 
 // Unless we have no one common component, we will be pass <Provider /> global redux
