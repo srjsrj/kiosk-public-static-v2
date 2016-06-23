8d34a1f6deecc1ad81e1a5e171b4477b08ecce60
Ananev Alexander <minoru.null@gmail.com>
2016-06-23 05:55:11 +0000

    фикс верстки товара в списке после добавления кнопки корзины

--- app/scripts/react/application.js
+++ app/scripts/react/application.js
@@ -18,7 +18,7 @@ if (global.gon.__data) {
 }
 
 global.Kiosk = {
-  version: '0.0.276',
+  version: '0.0.277',
 };
 
 // Unless we have no one common component, we will be pass <Provider /> global redux
