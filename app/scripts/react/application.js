d7fe7b8b9f83d08684a5cab773a410ecbd450f5e
Ananev Alexander <minoru.null@gmail.com>
2016-06-29 11:32:04 +0000

    fix disqus margin bottom

--- app/scripts/react/application.js
+++ app/scripts/react/application.js
@@ -18,7 +18,7 @@ if (global.gon.__data) {
 }
 
 global.Kiosk = {
-  version: '0.0.297',
+  version: '0.0.298',
 };
 
 // Unless we have no one common component, we will be pass <Provider /> global redux
