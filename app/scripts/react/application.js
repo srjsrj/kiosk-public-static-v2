08d58838c363000e9c3fc76b11e6dc869abe7937
Ananev Alexander <minoru.null@gmail.com>
2016-07-15 08:45:55 +0000

    фикс стрелок в слайдере на главной

--- app/scripts/react/application.js
+++ app/scripts/react/application.js
@@ -18,7 +18,7 @@ if (global.gon.__data) {
 }
 
 global.Kiosk = {
-  version: '0.0.332',
+  version: '0.0.333',
 };
 
 // Unless we have no one common component, we will be pass <Provider /> global redux
